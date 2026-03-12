9f6d07cc8d4cbe29b42169166438b22d552d205b
David Alves Dutra <dadutra@hotmail.com>
2026-03-12 11:23:08 -0300

    Projeto base. start.spring.io.

---
+++ src/main/java/com/dutra/empreende_sc/EmpreendeScApplication.java
@@ -0,0 +1,13 @@
+package com.dutra.empreende_sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreendeScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpreendeScApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dutra/empreende_sc/EmpreendeScApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.empreende_sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendeScApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
